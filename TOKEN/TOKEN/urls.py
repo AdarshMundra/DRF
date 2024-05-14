f6c666c8104337887f7061fe205d28f44e9d23d9
adarshmundra <adarsh.mundra1305@gmail.com>
2024-05-14 23:13:53 +0530

    Token

--- TOKEN/TOKEN/urls.py
+++ TOKEN/TOKEN/urls.py
@@ -18,13 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken.views import obtain_auth_token
+from api.auth import CustomAuthToken
 router = DefaultRouter()
 
-router.register( "studentapi",views.StudentModelViewSet, basename="Student")
-
+router.register("studentapi", views.StudentModelViewSet, basename="Student")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include(router.urls)),
+    path("", include(router.urls)),
+    path("gettoken/",obtain_auth_token),
+    path("gettoken1/", CustomAuthToken.as_view()),
 ]
